652accd9214415e62e446c73a7410a44c16a8392
JorgeDevmm <jorge29luis10@gmail.com>
2024-02-09 20:25:52 -0500

    Agregando función limpiarHTML

--- js/app.js
+++ js/app.js
@@ -84,10 +84,49 @@ function consultarAPI(ciudad, pais) {
   fetch(url)
     .then((respuesta) => respuesta.json())
     .then((datos) => {
-
-      console.log(datos);
+      // limpiar html duplicados de datos previo
+      limpiarHTML();
       if (datos.cod === '404') {
-        mostrarError(`Ciudad no encontrada`)
+        mostrarError(`Ciudad no encontrada`);
+        return;
       }
+
+      // imprimir la respuesta en el HTML
+      mostrarClima(datos);
     });
+}
+
+function mostrarClima(datosClima) {
+  const {
+    main: { temp, temp_max, temp_min },
+  } = datosClima;
+
+  // Convertir a centigrados
+  const centigrados = kelvinACentigrado(temp);
+
+  // creamos parrafos
+  const actual = document.createElement('p');
+  // insertamos el texto centigrados con inner por que usaremos con una entidad
+  actual.innerHTML = `${centigrados} &#8451;`;
+  actual.classList.add('font-bold', 'text-6xl');
+
+  const resultadoDiv = document.createElement('DIV');
+  resultadoDiv.classList.add('text-center', 'text-white');
+
+  // agregramos el parrafo actual a resultadoDiv
+  resultadoDiv.appendChild(actual);
+
+  // agregramos el resultadoDiv al resultado
+  resultado.append(resultadoDiv);
+}
+
+const kelvinACentigrado = (grados) => parseInt(grados - 272.15);
+
+function limpiarHTML() {
+  // iterar si se encuentra un elemento en resultado
+  while (resultado.firstChild) {
+    if (resultado.firstChild) {
+      resultado.removeChild(resultado.firstChild);
+    }
+  }
 }
